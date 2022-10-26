e8a70a214a418c48ea4fd1611ec2642522f4d6cb
paul <cherepaha325@gmail.com>
2022-10-27 00:32:35 +0300

    Updated JS-scripts

---
+++ JS-scripts/script.js
@@ -0,0 +1,29 @@
+/*
+1. Insert into values (man, 39);
+2. Dobrodeev Valera Vadimovich -> Dobrodeev V. V.;
+3. 10.01.1991 -> 10.01.91;
+4. 10/01/91 -> 10.01.91;
+5. parsing .html, sites Guzzle http, .js, .py;
+6. parsing .log, .json, .xml;
+7. regular expressions, A-parse
+ */
+let name = "Dobrodeev Valera Vadimovich",
+    newName = "",
+    data = "10.01.1991",
+    newData = "",
+    data2 = "10/01/91",
+    newData2 = "";
+
+let first_space = name.indexOf(' ');
+let last_space = name.lastIndexOf(' ');
+newName = name.slice(0, first_space) + ' ' + name.slice(first_space + 1, first_space + 2) + '. ' + name.slice(last_space + 1, last_space + 2) + '.';
+console.log(`${newName}\n`);
+
+let last_dot = data.lastIndexOf('.');
+newData = data.slice(0, last_dot + 1) + data.slice(-2);
+console.log(`${newData}\n`);
+
+let first_slash = data2.indexOf('/');
+let last_slash = data2.lastIndexOf('/');
+newData2 = data2.slice(0, first_slash) + '.' + data2.slice(first_slash + 1, last_slash) + '.' + data2.slice(last_slash + 1);
+console.log(newData2);
