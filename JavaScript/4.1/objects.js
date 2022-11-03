ed5c7d0734a9eb8647cdcebe3afe0add00bcc5c2
Anastasia Mamontova <mamontova.stasia@gmail.com>
2022-11-03 16:02:26 +0300

    Пришлось добавить еще 2 файла

---
+++ JavaScript/4.1/objects.js
@@ -0,0 +1,12 @@
+
+let user = {     // объект
+    name: "John",  // под ключом "name" хранится значение "John"
+    age: 30,        // под ключом "age" хранится значение 30
+    surname: "Smith",
+};
+/*
+alert( user.name ); // John
+alert( user.age ); // 30*/
+
+
+console.log(user.name,user.age);
