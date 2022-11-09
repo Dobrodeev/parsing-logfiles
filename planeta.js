3098769ba31100549198396efe59fd20b3509d94
Добродеев Валера <33656818+Dobrodeev@users.noreply.github.com>
2022-11-10 00:48:46 +0300

    создали обьект myPlanet.

--- planeta.js
+++ planeta.js
@@ -1,4 +1,15 @@
 let ourPlanetEarth, userName;
 nameOurPlanet="Земля";
 userName="John";
+let myPlanet = {
+    name: "Earth",
+    age: 6400,
+    speed: 30,
+    galaxy: "Milky way",
+    friend: "Moon",
+    getFriend: function(){
+        return this.friend;
+    }
+}
+console.log(myPlanet.getFriend());
 
