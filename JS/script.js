1a1c162942dc4cbaa0563712bf29381b4b319b66
Ivan <shestopal.i.p@gmail.com>
2022-11-16 09:55:23 +0200

    added calculator

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  min() {
+    return this.a - this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  div() {
+    if (this.b == 0) return "На 0 делить нельзя.";
+    else return this.a / this.b;
+  },
+
+  perc() {
+    return this.a - (this.a / 100 * this.b);
+  },
+
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
+};
+
+calculator.read();
+alert(`Результат: ${calculator.sum()}, ${calculator.min()}, ${calculator.mul()}, ${calculator.div()}, ${calculator.perc()}`);
