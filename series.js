e5586456746b2ffef679bd064c7cf50451dcf082
Коломаренко Дмитрій <kolobok575lg@gmail.com>
2022-11-19 18:03:01 +0200

    Додано розв'язки завдань Series23, Series25.

--- series.js
+++ series.js
@@ -47,12 +47,34 @@ Series23. Дано целое число N (> 2) и набор из N вещес
 Если данный набор является пилообразным, то вывести 0; в противном
 случае вывести номер первого элемента, не являющегося зубцом.
 */
-function series33() {
-	let numbers = [1, 2, 0, 4, 5, 6, 7, 8, 16, 0, 11, 16, 13, 0, 15, 23];
+function series23() {
+	//let numbers = [100, 1, 100, 1, 100, 1, 100, 1, 100, 1, 100, 1, 100, 1, 100, 1];
+	let numbers = [-100, 1, -100, 1, 100, 1, -100, 1, -100, 1, -100, 1, -100, 1, -100, 1];
 	
-	let index = 0;
+	let index              = 0;
+	let is_current_bigger  = true;
+	let is_current_smaller = true;
 	
-	for (let i = 0)
+	let is_saw = true;
+	
+	for (let i = 0; i + 1 < numbers.length; ++i) {
+		if (numbers[i] > numbers[i + 1] && is_current_bigger) {
+			is_current_smaller = true;
+			is_current_bigger  = false;
+		}
+		else if (numbers[i] < numbers[i + 1] && is_current_smaller) {
+			is_current_smaller = false;
+			is_current_bigger  = true;
+		}
+		else {
+			index = i;
+			is_saw = false;
+			break;
+		}
+	}
+	
+	alert(index);
+	alert(is_saw);
 }
 
 /**
@@ -84,6 +106,28 @@ function series24() {
 	alert(`Сума чисел між двома останніми нулями: ${sum}.`);
 }
 
+/**
+Series25. Дано целое число N и набор из N целых чисел, содержащий по
+крайней мере два нуля. Вывести сумму чисел из данного набора, распо-
+ложенных между первым и последним нулем (если первый и последний
+нули идут подряд, то вывести 0).
+*/
+function series25() {
+	let numbers = [1, 2, 0, 4, 5, 6, 7, 8, 16, 0, 11, 16, 13, 0, 15, 23];
+	
+	let first_zero = numbers.findIndex(element => element == 0);
+	let last_zero = numbers.lastIndexOf(0);
+	
+	let sum = 0;
+	for (let i = first_zero + 1; i < last_zero; ++i) {
+		sum += numbers[i];
+	}
+	
+	alert(sum);
+}
+
 //series16();
 //series22();
-series24();
+//series23();
+//series24();
+series25();
