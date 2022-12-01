dfe4b1771830c9f620ea37141d0c3701c4e67e97
Коломаренко Дмитрій <kolobok575lg@gmail.com>
2022-12-01 16:00:33 +0200

    Розв'язано задачі Proc32, Proc33.

--- proc.js
+++ proc.js
@@ -39,5 +39,67 @@ function proc3() {
 	alert(`AMean: ${result.AMean}; GMean: ${result.GMean}`);
 }
 
+/**
+Proc32. Описать функцию DegToRad(D) вещественного типа, находящую ве-
+личину угла в радианах, если дана его величина D в градусах (D — веще-
+ственное число, 0 < D < 360). Воспользоваться следующим соотношением:
+180° = π радианов. В качестве значения π использовать 3.14. С помощью
+функции DegToRad перевести из градусов в радианы пять данных углов.
+*/
+function proc32() {
+	function DegToRad(grades) {	
+		if (grades < 0 || grades > 360) {
+			throw Error("DegToRad: переданий параметр повнинен бути > 0 і <= 360");
+		}
+		
+		return (grades / 180) * Math.PI;
+	}
+	
+	for (let count = 5; count > 0; --count) {
+		let grade = getRandomArbitrary(10, 300);
+		console.log(`У градусах: ${grade.toFixed(2)}; у радіанах: ${DegToRad(grade).toFixed(2)}`);
+		alert(`У градусах: ${grade.toFixed(2)}; у радіанах: ${DegToRad(grade).toFixed(2)}`);
+	}
+	
+	try {
+		DegToRad(-8);
+	}
+	catch (error) {
+		alert(error.message);
+	}
+}
+
+/**
+Proc33. Описать функцию RadToDeg(R) вещественного типа, находящую вели-
+чину угла в градусах, если дана его величина R в радианах (R — вещест-
+венное число, 0 < R < 2·π). Воспользоваться следующим соотношением:
+180° = π радианов. В качестве значения π использовать 3.14. С помощью
+функции RadToDeg перевести из радианов в градусы пять данных углов.
+*/
+function proc33() {
+	function RadToDeg(radians) {
+		if (radians < 0 || radians > Math.PI * 2) {
+			throw Error("DegToRad: переданий параметр повнинен бути > 0 і <= 2·π");
+		}
+		
+		return (180 * radians) / Math.PI;
+	}
+	
+	for (let count = 5; count > 0; --count) {
+		let radian = getRandomArbitrary(0, Math.PI * 2);
+		console.log(`У радіанах: ${radian.toFixed(2)}; у градусах: ${RadToDeg(radian).toFixed(2)}`);
+		alert(`У радіанах: ${radian.toFixed(2)}; у градусах: ${RadToDeg(radian).toFixed(2)}`);
+	}
+	
+	try {
+		DegToRad(-8);
+	}
+	catch (error) {
+		alert(error.message);
+	}
+}
+
 //proc1();
-proc3();
+//proc3();
+//proc32();
+proc33();
