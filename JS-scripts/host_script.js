a76376c55d770315bca360a41dceef9d0d199ccc
paul <cherepaha325@gmail.com>
2022-12-09 20:48:56 +0200

    Added script for hosting local server and task with this

---
+++ JS-scripts/host_script.js
@@ -0,0 +1,18 @@
+const hostname = '127.0.0.1';
+const port = 3000;
+  console.log(`Server running at http://${hostname}:${port}/`);
+  let user = 'John';
+let age = 25;
+let message = 'Hello';
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...когда нам нужно выбрать цвет
+let color = COLOR_ORANGE;
+//alert(color); // #FF7F00
+const birthday = '18.04.1982';
+
+//const age = someCode(birthday);
+console.log(birthday);
