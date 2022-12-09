8b10a6fe6790c26ade8b842c1d905ad1efbbf3a1
Tidles <kolobok575lg@gmail.com>
2022-12-09 17:44:02 +0200

    Розв'язано задачі Minmax12, Minmax16, Minmax20.

--- minmax.js
+++ minmax.js
@@ -140,12 +140,98 @@ function minmax8() {
 	);
 }
 
+/**
+Minmax12◦. Дано целое число N и набор из N чисел
+Найти минимальное положительное число из данного набора.
+Если положительные числа в наборе отсутствуют, то вывести 0.
+*/
+function minmax12() {
+	let numbers = getRandomArray(-3, 3, 5, 1);
+	
+	let min_positive = Number.MAX_VALUE;
+	let is_positive_numbers = false;
+	for (let i = 0; i < numbers.length; ++i) {
+		if (min_positive > numbers[i] && numbers[i] > 0) {
+			min_positive = numbers[i];
+			
+			is_positive_numbers = true;
+		}
+	}
+
+	alert(
+		`Числа: ${numbers}\n` +
+		`Чи є додатні числа в масиві: ${(is_positive_numbers)? "так" : "ні"}\n` +
+		`Найменше додатнє число: ${min_positive}`
+	);
+}
+
+/**
+Minmax16. Дано целое число N и набор из N целых чисел.
+Найти количество элементов, расположенных перед первым минимальным элементом.
+*/
+function minmax16() {
+	let numbers = getRandomArray(0, 5, 8, 0);
+	
+	const min_element = Math.min(...numbers);
+	// оскільки індексування масивів починаєттся з 0, то нічого рахувати не треба,
+	// тобто first_index_of_min є кількістю елементів перед першим найменшим числом
+	const first_index_of_min = numbers.findIndex(num => num == min_element);
+	
+	alert(
+		`Числа: ${numbers}\n` +
+		`Найменше число: ${min_element}\n` +
+		`Перший індекс наймешого числа: ${first_index_of_min}\n` +
+		`Кількість елементів перед першим найменшим числом: ${first_index_of_min}`
+	);
+}
+
+/**
+Minmax20. Дано целое число N и набор из N целых чисел.
+Найти общее количество экстремальных (то есть минимальных и максимальных) элементов из данного набора.
+*/
+function minmax20() {
+	let numbers = getRandomArray(-7, 7, 15, 0);
+	
+	let minimum = Math.min(...numbers);
+	let maximum = Math.max(...numbers);
+	
+	let num_of_mins = 0;
+	numbers.forEach(
+		num => {
+			if (num == minimum) {
+				++num_of_mins;
+			}
+		}
+	);
+	
+	let num_of_maxs = 0;
+	numbers.forEach(
+		num => {
+			if (num == maximum) {
+				++num_of_maxs;
+			}
+		}
+	);
+	
+	let num_of_extrems = num_of_mins + num_of_maxs;
+	
+	alert(
+		`Числа: ${numbers}\n` +
+		`Найменше число: ${minimum}\n` +
+		`Найбільше число: ${maximum}\n` +
+		`Кількість екстремальних чисел: ${num_of_extrems}`
+	);
+}
+
 try {
 	//minmax1();
 	//minmax2_3();
 	//minmax4();
 	//minmax5();
-	minmax8();
+	//minmax8();
+	//minmax12();
+	//minmax16();
+	minmax20();
 }
 catch (error) {
 	alert(error.message);
